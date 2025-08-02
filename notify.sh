8f2fa413a35e9c010fde03dd19cac430cefc346b
dimanzver <diman.suvor.linda.2000@gmail.com>
2025-08-02 12:07:13 +0300

    fix in crontab

--- notify.sh
+++ notify.sh
@@ -3,7 +3,7 @@
 title=$1
 url=$2
 notify_title="Конфликт в PR $title"
-pressed_button="$(notify-send "$notify_title" --action "to_github=Перейти в Github")"
+pressed_button="$(XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send "$notify_title" --action "to_github=Перейти в Github")"
 
 if [[ "$pressed_button" == "to_github" ]]; then
   google-chrome-stable "$url"
